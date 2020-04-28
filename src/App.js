a8f2d92778c04f40034674814d3cc5ee0ab81b24
Dan Johnson <danimal.johnson@gmail.com>
2020-04-27 17:01:22 -0700

    Initial React app.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is our new, spiffy app.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
