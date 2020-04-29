3ced12754ac0f0aa12690ad34bb9eb153c88ecb7
Dan Johnson <danimal.johnson@gmail.com>
2020-04-29 10:55:46 -0700

    Basic structure and styling.
    chore: add component files.
    styling: add eslint dependencies.
    chore: code cleanup.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'
-import { BrowserRouter as Router } from 'react-router-dom'
+import logger from 'redux-logger';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { reducer } from './reducers';
 
-const store = createStore(reducer, applyMiddleware(thunk, logger))
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-<Provider store={store} >
+  <Provider store={store}>
     <Router>
-        <App />
+      <App />
     </Router>
-</Provider>
-, document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,53 +7,54 @@ import {
   // LOGOUT_FAILURE
   REGISTER_START,
   REGISTER_SUCCESS,
-  REGISTER_FAILURE
+  REGISTER_FAILURE,
 } from '../actions';
 
 const initialState = {
   loggedInUsername: '',
   isLoading: false,
-  error: ''
-}
+  error: '',
+};
 
-export const reducer = ( state = initialState, action ) => 
-{
-  switch(action.type) {
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case LOGIN_START:
       return {
         ...state,
         isLoading: true,
-      }     
+      };
     case LOGIN_SUCCESS:
       return {
         ...state,
         loggedInUsername: action.payload,
         isLoading: false,
-      }
+      };
     case LOGIN_FAILURE:
       return {
-          ...state,
-          isLoading: false,
-          error: action.payload
-        }
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
     case REGISTER_START:
       return {
         ...state,
         isLoading: true,
-      }     
+      };
     case REGISTER_SUCCESS:
       return {
         ...state,
         loggedInUsername: action.payload,
         isLoading: false,
-      }
+      };
     case REGISTER_FAILURE:
       return {
         ...state,
         isLoading: false,
-        error: action.payload
-      }          
+        error: action.payload,
+      };
     default:
       return state;
   }
-}
+};
+
+export default reducer;
