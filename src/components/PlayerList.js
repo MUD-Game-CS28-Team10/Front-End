ead2fb153976f71f600338e27903972a4f382ae4
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-01 11:43:25 -0500

    Code cleanup.

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,16 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Title, Content } from '../styledComponents/styles';
 
 const PlayerList = (props) => {
-  // const { playersArray } = props;
-
-  console.log('PlayersArray', props.playersArray);
+  const { playersArray } = props;
 
   return (
     <div className="player-list">
       <h2>Player List</h2>
-      {props.playersArray && props.playersArray.map(player => (
+      {playersArray && playersArray.map(player => (
         <p>{player}</p>
       ))}
     </div>
